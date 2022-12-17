3f885454c4a290756cc371abb01189fe8705d7cb
saka94 <74228457+saka94@users.noreply.github.com>
2022-12-17 15:23:04 +0100

    fixed video on mobile

--- src/components/login/LoginVideo/LoginVideo.jsx
+++ src/components/login/LoginVideo/LoginVideo.jsx
@@ -2,5 +2,5 @@ import clip from "../../../Video/pexels-artem-podrez-7774457.mp4";
 import styles from "./LoginVideo.module.css";
 
 export default function LoginVideo() {
-  return <video src={clip} autoPlay loop className={styles.backgroundVideo}></video>;
+  return <video src={clip} autoPlay loop playsInline className={styles.backgroundVideo}></video>;
 }
